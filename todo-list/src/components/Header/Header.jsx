47c7d4ef51e9713717382d66b1b005105b498ea9
김용민 <dydals3440@gmail.com>
2023-03-04 21:51:06 +0900

    style: Header

--- todo-list/src/components/Header/Header.jsx
+++ todo-list/src/components/Header/Header.jsx
@@ -1,13 +1,21 @@
 import React from "react";
+import styles from "./Header.module.css";
 
 export default function Header({ filters, filter, onFilterChange }) {
   return (
-    <header>
-      <ul>
+    <header className={styles.header}>
+      <ul className={styles.filters}>
         {/* 동적으로 리스트요소가 변경되는 것이아니라 고정도니 ui이니 인덱스 사용 */}
         {filters.map((value, index) => (
           <li key={index}>
-            <button onClick={() => onFilterChange(value)}>{value}</button>
+            <button
+              //
+              className={`${styles.filter} 
+              ${filter === value && styles.selected}`}
+              onClick={() => onFilterChange(value)}
+            >
+              {value}
+            </button>
           </li>
         ))}
       </ul>
